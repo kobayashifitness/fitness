a50e0258690510210af1c3a13e89a4d29ade22d6
idealexe <ideal.exe@live.jp>
2016-09-04 08:12:08 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsMuscle::Application.config.secret_key_base = 'c5d69babc81791658fad93c9dca3d87480586706d32180031db5660f2ea228519d6583241e8594158da303023372c7cafd3850281bfeedb20b72d86ff86ab0e1'
