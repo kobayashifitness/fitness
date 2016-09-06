9aacb089c5037f196785ab9221eda57668521d22
idealexe <ideal.exe@live.jp>
2016-09-06 17:18:57 +0000

    scaffoldTest

---
+++ app/views/personals/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @personal, :id, :age, :datetime, :fat, :goal, :height, :muscle, :name, :sex, :weight, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904082951) do
+ActiveRecord::Schema.define(version: 20160906165305) do
+
+  create_table "personals", force: true do |t|
+    t.integer  "age"
+    t.string   "datetime"
+    t.float    "fat"
+    t.string   "goal"
+    t.float    "height"
+    t.float    "muscle"
+    t.string   "name"
+    t.string   "sex"
+    t.float    "weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
