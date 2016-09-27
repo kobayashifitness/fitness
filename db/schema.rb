d17a3ad43c819c5a45ba75d8e1300988da63a9f9
kobayshi <hk5884.2.5@gmail.com>
2016-09-27 07:05:41 +0000

    create mymenu model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913053033) do
+ActiveRecord::Schema.define(version: 20160927070445) do
 
   create_table "diaries", force: true do |t|
     t.float    "weight"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160913053033) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "event"
+  end
+
+  create_table "mymenus", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "personals", force: true do |t|
