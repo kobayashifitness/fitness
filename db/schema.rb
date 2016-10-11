e91d5fe718e77a0eff23507a5aad93214769723c
kobayahsi <kobayahsi.hiroki@ichikawa-lab.org>
2016-10-11 06:41:48 +0000

    input db

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927083143) do
+ActiveRecord::Schema.define(version: 20161011062129) do
 
   create_table "events", force: true do |t|
     t.string   "event_name"
-    t.string   "class"
+    t.string   "event_class"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
