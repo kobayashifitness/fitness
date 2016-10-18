e4c27356912e2a2e5d9860bb367b00a2b0d3a78e
kobayahsi <kobayahsi.hiroki@ichikawa-lab.org>
2016-10-18 08:19:44 +0000

    目標の入力

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,10 +6,26 @@ class HomesController < ApplicationController
     @year_goal = current_user.goals.where(span: 'year' ).last
     @month_goal = current_user.goals.where(span: 'month').last
     @week_goal = current_user.goals.where(span: 'week').last
+    if @year_goal == nil
+      @year_goal = Goal.new
+      @year_goal.user_id = current_user.id
+      @year_goal.goal = "今年の目標を設定してください"
+    end
+    if @month_goal == nil
+      @month_goal = Goal.new
+      @month_goal.user_id = current_user.id
+      @month_goal.goal = "今月の目標を設定してください"
+    end
+    if @week_goal == nil
+      @week_goal = Goal.new
+      @week_goal.user_id = current_user.id
+      @week_goal.goal = "今年の目標を設定してください"
+    end
 
     t = Time.now
     @date = t.strftime("%Y-%m-%d")
 
+    @goal = Goal.new
     @muscle_diary = MuscleDiary.new
     @muscle_mass = MuscleMass.new
     @height = Height.new
