d92aa5a1ef9744d8bc5510bb55e4b1f930403809
idealexe <omega_8810@yahoo.co.jp>
2016-10-21 02:53:43 +0900

    体重データがない場合に対応

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -4,11 +4,15 @@ class GraphsController < ApplicationController
   respond_to :html
 
   def index
-    @weights = Weight.all
-    @max_weight = @weights.maximum('weight')
-    @min_weight = @weights.minimum('weight')
-    #@graph = @weights.map{|w| [w.diary_date, w.weight]} # weights内の個々のweightについて[日付，体重]の配列にマッピング
-    @graph = @weights.pluck(:diary_date, :weight) # pluckの方がよかった
+    @profile = current_user.profile
+    @weights = Weight.where(profile_id: @profile.id)  #現在のユーザーのweightデータを取り出す
+    if !@weights.empty?
+      @max_weight = @weights.maximum('weight')
+      @min_weight = @weights.minimum('weight')
+      #@graph = @weights.map{|w| [w.diary_date, w.weight]} # weights内の個々のweightについて[日付，体重]の配列にマッピング
+      @graph = @weights.pluck(:diary_date, :weight) # pluckの方がよかった
+    end
+
   end
 
 end
