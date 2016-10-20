851c77df03794a69021707a281be834368c509c1
idealexe <omega_8810@yahoo.co.jp>
2016-10-21 02:12:38 +0900

    配列の切り出しをpluckに変更

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -7,7 +7,8 @@ class GraphsController < ApplicationController
     @weights = Weight.all
     @max_weight = @weights.maximum('weight')
     @min_weight = @weights.minimum('weight')
-    @graph = @weights.map{|w| [w.diary_date, w.weight]} # weights内の個々のweightについて[日付，体重]の配列にマッピング
+    #@graph = @weights.map{|w| [w.diary_date, w.weight]} # weights内の個々のweightについて[日付，体重]の配列にマッピング
+    @graph = @weights.pluck(:diary_date, :weight) # pluckの方がよかった
   end
 
 end
